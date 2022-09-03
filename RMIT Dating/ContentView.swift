18ec6584297fafbadd1e09df2fb47f07580fcea8
DaiHoang <hoangquocdai1411@gmail.com>
2022-09-03 14:00:11 +0700

    Initial Commit

---
+++ RMIT Dating/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RMIT Dating
+//
+//  Created by Tri Nguyen on 03/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RMIT Dating/RMIT_DatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_DatingApp.swift
+//  RMIT Dating
+//
+//  Created by Tri Nguyen on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_DatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
