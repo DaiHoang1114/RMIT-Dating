dd7ad7c3d5829a257ebfdb2893f1aac31adecec2
Khoi7 <s3694615@rmit.edu.vn>
2022-09-15 21:19:36 +0700

    first part of setting wizard

--- RMIT Dating/Views/SettingView.swift
+++ RMIT Dating/Views/SettingView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct SettingView: View {
+    var dateFormat: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }
+    
+    @State var username: String = ""
+    @State var birthday = Date()
+    @State var email: String = ""
+    @State var phone: String = ""
+    
     var body: some View {
-        ZStack {
-            Color.green
-            Text("Setting View")
-                .foregroundColor(Color.white)
+        VStack {
+            Form {
+                TextField(text: $username) {
+                    Text("Username")
+                }
+                DatePicker(selection: $birthday, in: ...Date(), displayedComponents: .date) {
+                    Text("Birthday")
+                        .foregroundColor(.gray)
+                }
+                .fixedSize()
+                TextField(text: $email) {
+                    Text("Email")
+                }
+                TextField(text: $phone) {
+                    Text("Phone")
+                }
+                Section{
+                    Button("Next") {
+                        
+                    }
+                    .frame(width: .infinity, alignment: .center)
+                }
+                
+            }
         }
     }
 }
