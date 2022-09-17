2245bafb8694cd8b7bfc541842bdca63ab19239e
dmnhat91 <henry.duong02@gmail.com>
2022-09-17 11:06:28 +0700

    Refactor UserInfoView

--- RMIT Dating/Models/ColorConstants.swift
+++ RMIT Dating/Models/ColorConstants.swift
@@ -10,4 +10,6 @@ import SwiftUI
 
 struct ColorConstants{
     static let lightGreyColor = Color(red: 0.831, green: 0.831, blue: 0.831)
+    
+    static let lightBlueGreyColor = Color("LightBlueGrey");
 }
--- RMIT Dating/Views/UserInfoView.swift
+++ RMIT Dating/Views/UserInfoView.swift
@@ -9,29 +9,39 @@ import Foundation
 import SwiftUI
 
 struct UserInfoView: View {
+    var name: String = "name"
+    var age: String = "age"
+    
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    CircleImage(image: Image("avatar-sample"))
-                    
-                    Text("Name, Age")
-                    
-                    NavigationLink {
-                        SettingView()
-                        } label: {
-                            Text("Edit profile")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                                .frame(width: 220, height: 60)
-                                .background(Color.green)
-                                .cornerRadius(15.0)
-                        }
-                }
+            VStack {
+                ZStack {
+                    VStack {
+                        CircleImage(image: Image("avatar-sample"))
+                        
+                        Text("\(name), \(age)".uppercased())
+                            .font(.system(size: 35, weight: .bold))
+                        
+                        
+                        HStack {
+                            CircleButton(image: Image(systemName: "gear"), text: "settings")
+                            
+                            NavigationLink {
+                                SettingView()
+                            } label: {
+                                CircleButton(image: Image(systemName: "pencil"), text: "edit profile")
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            
+                            CircleButton(image: Image(systemName: "checkerboard.shield"), text: "safety")
+                        } // endHStack
+                    } // end VStack
+                } //end ZStack
                 
-            }
-        }
+                Spacer()
+            } //end VStack
+        } //end NavigationView
 
     }
 }
@@ -39,5 +49,6 @@ struct UserInfoView: View {
 struct UserInfoView_Previews: PreviewProvider {
     static var previews: some View {
         UserInfoView()
+//        UserInfoView(name: "Jisoo", age: "27")
     }
 }
