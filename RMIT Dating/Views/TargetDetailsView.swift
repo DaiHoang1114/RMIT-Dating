967e1bce8108a66685a955c330d0b25f786d9b4f
DaiHoang <hoangquocdai1411@gmail.com>
2022-09-17 14:46:26 +0700

    Create TargetDetailsView

---
+++ RMIT Dating/Views/TargetDetailsView.swift
@@ -0,0 +1,107 @@
+//
+//  TargetDetailsView.swift
+//  RMIT Dating
+//
+//  Created by Tri Nguyen on 17/09/2022.
+//
+
+import SwiftUI
+
+struct TargetDetailsView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                ImageScrollView()
+                
+                VStack {
+                    HStack {
+                        Text("Name")
+                            .fontWeight(.bold)
+                            .font(.system(size: 30))
+                        Text("18")
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Image(systemName: "location.fill")
+                        Text("6")
+                        Text("kilometers away")
+                        Spacer()
+                    }
+                    .padding(.bottom, 20)
+                    HStack {
+                        Text("About Me")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 23))
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Looking for chat, friendship")
+                        Spacer()
+                    }
+                    .padding(.bottom, 20)
+                    
+                    HStack {
+                        Text("My Interests")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 23))
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Coffee")
+                            .foregroundColor(.gray)
+                            .padding(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(.gray, lineWidth: 2)
+                            )
+                        Text("Music")
+                            .foregroundColor(.gray)
+                            .padding(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(.gray, lineWidth: 2)
+                            )
+                        Text("Cooking")
+                            .foregroundColor(.gray)
+                            .padding(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(.gray, lineWidth: 2)
+                            )
+                        Spacer()
+                    }
+                    .padding(.bottom, 20)
+                }
+                .padding(.leading, 20)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ImageScrollView : View {
+    var body: some View {
+        Text("ImageScrollView")
+            .frame(minWidth: 0, idealWidth: 180, maxWidth: .infinity, minHeight: 200, idealHeight: 350, maxHeight: 400, alignment: .center)
+    }
+}
+
+
+struct AboutMeView : View {
+    var body: some View {
+        Text("AboutMeView")
+    }
+}
+
+struct MyInterestsView : View {
+    var body: some View {
+        Text("MyInterestsView")
+    }
+}
+
+struct TargetDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TargetDetailsView()
+    }
+}
